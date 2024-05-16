8722c863574268d0301e625ab4badfd67c640e85
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-05-16 16:13:35 -0300

    feat: add splashcreen

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Logo from "@assets/logo.svg";
 
 import * as S from './styles';
 
 export function SplashScreen() {
   return (
     <S.Container>
+      <Logo />
 
+      {/* <S.CircleOne /> */}
     </S.Container>
   );
 }
--- src/screens/SplashScreen/styles.ts
+++ src/screens/SplashScreen/styles.ts
@@ -1,5 +1,27 @@
+import { Dimensions } from "react-native";
 import styled from "styled-components/native";
 
+const { width, height } = Dimensions.get("screen");
+
 export const Container = styled.View`
+  position: relative;
   flex: 1;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  /* background: ${({ theme }) => theme.colors.main}; */
+`;
+
+export const CircleOne = styled.View`
+  position: absolute;
+  top: 0;
+  right: -${width / 6}px;
+
+  width: 238px;
+  height: 238px;
+  border-radius: 124px;
+
+  background: ${({ theme }) => theme.colors.warning};
 `;
